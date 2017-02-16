fbf0eaba55cbbc18a2278480a17e880aeaa133bf
Javier Carrillo <javicarrillo83@gmail.com>
2017-02-16 11:18:08 +0100

    - Map visualization with clustering.
    - What if

---
+++ map-test/js/realworld.389.js
@@ -0,0 +1,12 @@
+
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var addressPoints = [
+[41.38506389999999, 2.1734034999999494, "Barcelona 1"]
+[41.38506389999999, 2.1734034999999494, "Barcelona 2"]
+[41.38506389999999, 2.1734034999999494, "Barcelona 3"]
+[41.38506389999999, 2.1734034999999494, "Barcelona 4"]
+[41.38506389999999, 2.1734034999999494, "Barcelona 5"]
+[41.38506389999999, 2.1734034999999494, "Barcelona 6"]
+                
+];
