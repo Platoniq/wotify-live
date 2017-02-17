edd7074d7570bc4cb5cfd8cb9fccf93e9745ee70
Ivan Vergés <ivan@microstudi.net>
2017-02-17 16:28:40 +0100

    Live admin on

--- Server.js
+++ Server.js
@@ -1,10 +1,10 @@
 var express   = require("express");
 var app       = express();
 var http      = require('http').Server(app);
-var io        = require("socket.io")(http);
 var pug       = require('pug');
 var less      = require('less-middleware');
 var routes    = require('lib/routes.js');
+var live = require('lib/live.js');
 
 // Setup template engine
 app.set('views', __dirname + '/views');
@@ -18,47 +18,8 @@ app.use('/', routes);
 // Handle public assets
 app.use(express.static(__dirname + '/public'));
 
-
-/*  This is auto initiated event when Client connects to Your Machien.  */
-
-io.on('connection',function(socket){
-    console.log("A user is connected");
-    socket.on('status added',function(status){
-      add_status(status,function(res){
-        if(res){
-            io.emit('refresh feed',status);
-        } else {
-            io.emit('error');
-        }
-      });
-    });
-    setTimeout(function(){
-      io.emit('refresh feed','Test ' + new Date());
-    }, 1000);
-    setTimeout(function(){
-      io.emit('refresh feed','Test ' + new Date());
-    }, 2000);
-});
-
-var add_status = function (status,callback) {
-  console.log('add status',status);
-    pool.getConnection(function(err,connection){
-        if (err) {
-          callback(false);
-          return;
-        }
-    connection.query("INSERT INTO `status` (`s_text`) VALUES ('"+status+"')",function(err,rows){
-            connection.release();
-            if(!err) {
-              callback(true);
-            }
-        });
-     connection.on('error', function(err) {
-              callback(false);
-              return;
-        });
-    });
-}
+// Live (socket.io) features
+live(app, http);
 
 http.listen(3000,function(){
     console.log("Listening on 3000");
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,12 +1,11 @@
-$(function(){
-      var socket = io();
-      $("#add_status").click(function(){
-        socket.emit('status added',$("#comment").val());
-      });
-      socket.on('refresh feed',function(msg){
-        $("#show_comments").append(msg + '<br /><br />');
-      });
-      socket.on('refresh page',function(page){
-        location = page;
-      });
+
+// Global var
+SOCKET = io();
+AUTORELOAD = true;
+
+// Common tasks
+SOCKET.on('reload page',function(page){
+  if(AUTORELOAD) {
+    location.reload();
+  }
 });
