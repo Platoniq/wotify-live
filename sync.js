8bc6aa37413ac23eb22455d5b740a2b597921f16
Ivan Vergés <ivan@microstudi.net>
2017-02-24 13:34:23 +0100

    fix sync

--- sync.js
+++ sync.js
@@ -24,25 +24,36 @@ console.log('Using URL %s', program.url);
 
 var api = require(__dirname + '/lib/api.js')(program.url);
 
+POLLING=false;
+
 if(program.interval) {
   var poll = '*/' + program.interval + ' * * * * *'
   console.log('Polling every %d seconds',program.interval);
-  new CronJob(poll, function(){pollProjects(false)}, null, true);
+  new CronJob(poll, function(){
+    if(POLLING) {
+      console.error('ALREADY POLLING, ABORTING');
+    } else {
+      pollProjects(false);
+    }
+  }, null, true);
 } else {
   pollProjects(true);
 }
 
-var allProjects = [];
-var allUsers = [];
+function abort(exit, msg, err) {
+  POLLING=false;
+  if(err) console.log(msg, err);
+  if(exit) process.exit(1);
+  else return;
+}
 
 function pollProjects(exit) {
+  var allProjects = [];
+  var allUsers = [];
   console.log('Sync users...');
+  POLLING=true;
   api.request('/users', 0, function(err, data){
-      if(err) {
-        console.log('ERROR', err);
-        if(exit) process.exit(1);
-        else return;
-      }
+      if(err) return abort(exit, 'ERROR', err);
       console.log('%d results, going to next page...', data.length);
       allUsers = _.union(allUsers, data);
   }, function(err, data){
@@ -50,14 +61,14 @@ function pollProjects(exit) {
 
     var calls = [];
 
-    allUsers.forEach(function(u){
+    allUsers.forEach(function(u, index){
       calls.push(function(callback){
         var user = {id: u._id, userId:u.userId, name:u.name, role:u.role, bio:u.bio, avatar: u.picture};
         if(u.email) {
           user.email = u.email;
         }
         models.User.findOneAndUpdate({id: user.id},user,{upsert:true},function(){
-          console.log('Imported user', user.userId,user.name);
+          console.log(index + ' - Imported user', user.userId,user.name,user.id);
           callback();
         });
       });
@@ -65,33 +76,21 @@ function pollProjects(exit) {
     });
 
     async.parallel(calls, function(err,result){
-      if(err) {
-        console.log('ERROR', err);
-        if(exit) process.exit(1);
-        else return;
-      }
+      if(err) return abort(exit, 'ERROR', err);
       console.log('Done importing %d users',result.length);
 
       console.log('Getting projects...');
 
       api.request('/projects', 0, function(err, data){
-        if(err) {
-          console.log('ERROR', err);
-          if(exit) process.exit(1);
-          else return;
-        }
-            console.log('%d results, going to next page...', data.length);
+        if(err) return abort(exit, 'ERROR', err);
+        console.log('%d results, going to next page...', data.length);
         allProjects = _.union(allProjects, data);
       }, function(){
         console.log('Obtained %d projects, now filtering', allProjects.length);
         console.log('Getting configured steps');
 
         models.Step.find().exec(function(err, steps){
-          if(err) {
-            console.log('ERROR', err);
-            if(exit) process.exit(1);
-            else return;
-          }
+          if(err) return abort(exit, 'ERROR', err);
           steps.forEach(function(step) {
 
             var projects = api.filterProjects(step.step, allProjects);
@@ -103,23 +102,16 @@ function pollProjects(exit) {
             console.log('Step %d reduced to %d projects, saving to database', step.step, projects.length );
 
             models.Slide.findOne({step:step.step}, function(err, slide){
-              if(err) {
-                console.log('ERROR FIND SLIDE', err);
-                if(exit) process.exit(1);
-                else return;
-              }
+              if(err) return abort(exit, 'ERROR FIND SLIDE', err);
               if(!slide) slide = new models.Slide({step: step.step});
-              //TODO: split multiple answers
-              slide.slides = projects;
+              var notes = _.filter(slide.slides, function(s){
+                return s.type === 'note';
+              });
+              slide.slides = _.union(notes, projects);
               slide.save(function(err, slide){
-                if(err) {
-                  console.log('ERROR SAVING SLIDE', err);
-                  if(exit) process.exit(1);
-                  else return;
-                }
+                if(err) return abort(exit, 'ERROR SAVING SLIDE', err);
                 console.log('Saved slide %d with %d slides', slide.step,slide.slides.length);
-                if(exit) process.exit();
-                else return;
+                return abort(exit);
               })
             });
           });
