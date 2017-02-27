436f356f7238d6182f860bd2ef5001b31d88b027
Ivan Vergés <ivan@microstudi.net>
2017-02-27 15:23:22 +0100

    fullscreen options

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,37 +10,10 @@ SOCKET.on('reload page',function(page){
   }
 });
 
-// Notifications
-function showSuccess(txt) {
+function showMsg(txt, type, delay) {
   $.notify(txt, {
-    type:'success',
-    animate: {
-      enter: 'animated bounceInDown',
-      exit: 'animated bounceOutUp'
-    }
-  });
-}
-function showError(txt) {
-  $.notify(txt, {
-    type:'danger',
-    animate: {
-      enter: 'animated bounceInDown',
-      exit: 'animated bounceOutUp'
-    }
-  });
-}
-function showInfo(txt) {
-  $.notify(txt, {
-    type:'info',
-    animate: {
-      enter: 'animated bounceInDown',
-      exit: 'animated bounceOutUp'
-    }
-  });
-}
-function showWarning(txt) {
-  $.notify(txt, {
-    type:'warning',
+    type: type || 'info',
+    delay: delay || 5000,
     animate: {
       enter: 'animated bounceInDown',
       exit: 'animated bounceOutUp'
@@ -48,6 +21,37 @@ function showWarning(txt) {
   });
 }
 
+// Notifications
+function showSuccess(txt, delay) {
+  showMsg(txt, 'success', delay);
+}
+function showError(txt, delay) {
+  showMsg(txt, 'danger', delay);
+}
+function showInfo(txt, delay) {
+  showMsg(txt, 'info', delay);
+}
+function showWarning(txt, delay) {
+  showMsg(txt, 'warning', delay);
+}
+
 function getHexagon(user) {
   return '<div class="avatar hexagon" style="background-image:url(' + user.avatar + ')" title="' + user.title + '"><div class="hex-top"></div><div class="hex-bottom"></div><div class="role role-' + user.role + '"></div></div>';
 }
+
+
+function toggleFullscreen() {
+  if($.fullscreen.isFullScreen())
+    $.fullscreen.exit();
+  else
+    $('html').fullscreen();
+}
+
+$(function(){
+  $("body").keypress(function(e){
+      // alert(e.which);
+      if(e.which == 102) toggleFullscreen();
+  });
+  $('body').on('.goto-fullscreen', 'click', toggleFullscreen);
+  showInfo('Press "f" for awesomeness', 1000);
+});
